a24f0698274ddd83b18f98807ee4bd25c1aaf1d6
Bruno Martins <brmartins92@gmail.com>
2022-08-07 17:03:38 -0300

    feat: ajust

---
+++ src/component/button/index.js
@@ -0,0 +1,15 @@
+
+import './style.css';
+
+function Button() {
+  return (
+    <div className="App">
+      <button>
+        <p>H</p>
+        <p>hidrogenio</p>
+      </button>
+    </div>
+  );
+}
+
+export default Button;
